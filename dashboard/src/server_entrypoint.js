fea02d98ce650c3668babb2a6619a33114b3559f
EricB <ebergeron@websimhockey.com>
2025-08-25 11:52:09 -0400

    supprimer dossier src

--- dashboard/src/server_entrypoint.js
+++
@@ -1,8 +0,0 @@
-function doGet() {
-  return HtmlService.createTemplateFromFile('ui').evaluate()
-    .setTitle('Suivi Inscriptions – Dashboard v0.1')
-    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
-}
-function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename).getContent();
-}
